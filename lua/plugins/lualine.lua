2249f61365a5b5efcc125a984b75507fb97ad7a6
Park Changbeom <eric9710@naver.com>
2025-08-10 00:31:20 +0900

    fix: lualine lazy load 적용

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,11 @@
 return {
-  'lualine.nvim',
+  "lualine.nvim",
   enabled = not vim.g.vscode,
+  event = "DeferredUIEnter",
   before = function()
     require("lz.n").trigger_load({ "nvim-web-devicons" })
   end,
   after = function()
-    require('lualine').setup()
-  end
+    require("lualine").setup()
+  end,
 }
