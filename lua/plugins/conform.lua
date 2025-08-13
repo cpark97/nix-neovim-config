8d18ce0dd51421dbabd9a302b1051474ae73632b
Park Changbeom <eric9710@naver.com>
2025-08-14 00:37:12 +0900

    fix: conform lsp fallback 디폴트로 설정

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -21,6 +21,9 @@ return {
 
     require("conform").setup({
       formatters_by_ft = formatters_by_ft,
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
       format_on_save = {
         lsp_format = "fallback",
       },
