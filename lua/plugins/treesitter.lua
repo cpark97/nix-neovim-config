621424496686b3a6d2141cf838410a4aa052a315
Park Changbeom <eric9710@naver.com>
2026-01-17 17:07:49 +0900

    update: nixpkgs, nixCats, nvim-treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,43 +3,30 @@ return {
     "nvim-treesitter",
     event = "DeferredUIEnter",
     after = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        -- ensure_installed = {
-        --   "lua",
-        --   "luadoc",
-        --   "bash",
-        --   "diff",
-        --   "html",
-        --   "javascript",
-        --   "typescript",
-        --   "tsx",
-        --   "json",
-        --   "yaml",
-        --   "toml",
-        --   "make",
-        --   "markdown",
-        --   "vim",
-        --   "vimdoc",
-        -- },
-        -- auto_install = true,
-        highlight = { enable = not vim.g.vscode },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<CR>",
-            scope_incremental = "<CR>",
-            node_incremental = "<TAB>",
-            node_decremental = "<S-TAB>",
-          },
-        },
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+          local lang = vim.treesitter.language.get_lang(filetype)
+
+          if vim.treesitter.language.add(lang) then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+            if not vim.g.vscode then
+              vim.treesitter.start(args.buf, lang)
+              vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+              vim.wo.foldmethod = "expr"
+            end
+          end
+        end,
       })
     end,
   },
   {
-    "nvim-treesitter-textobjects",
+    "nvim-treesitter-textobjects", -- @see: mini.lua
     event = "DeferredUIEnter",
+    beforeAll = function()
+      vim.g.no_plugin_maps = true
+    end,
     before = function()
       require("lz.n").trigger_load("nvim-treesitter")
     end,
